b2ec9d8b00ea7db72ce7a05d8b72d92373f1bdac
Lakshita <97162780+lakshitaa11@users.noreply.github.com>
2024-07-18 03:50:45 +0530

    Update Navbar.tsx

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
     }, 3000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [octopusImages.length]);
 
   return (
     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
