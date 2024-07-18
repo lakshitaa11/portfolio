3bf40603ba78041fb7476ca6d2bea112198610b8
Lakshita <97162780+lakshitaa11@users.noreply.github.com>
2024-07-19 03:10:27 +0530

    Update SkillText.tsx

--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -20,7 +20,7 @@ const SkillText = () => {
         variants={slideInFromLeft(0.5)}
         className='text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]'
         >
-            &quot;Develop, Design, Deliver&quot;
+            Develop, Design, Deliver
         </motion.div>
         <motion.div
         variants={slideInFromRight(0.5)}
